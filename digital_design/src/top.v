b3684156aca38a5e439c1260b4c2def12c8e2e3e
BryantSuen <BryantSuen@163.com>
2021-06-28 16:57:07 +0800

    fix error

--- digital_design/src/top.v
+++ digital_design/src/top.v
@@ -3,7 +3,7 @@ module top(system_clk,reset,cathode,stimulus,leds);
 
 input system_clk,reset,cathode;
 output stimulus;
-output[11:0] leds;
+output reg[11:0] leds;
 
 wire[11:0] show;
 wire clk_34,clk_17k;
@@ -13,7 +13,7 @@ clock clock_1(.system_clk(system_clk),.reset(reset),
               .clk_1k(clk_1k),.clk_17k(clk_17k),
               .stimulus(stimulus),.clk_34(clk_34));
 
-counter counter_1(.ena(ena),.reset(reset),
+counter counter_1(.ena(ena),.reset(clk_34),
                   .clk(clk_17k),.show(show));
 
 always @(posedge cathode or posedge reset or posedge clk_34)
@@ -28,5 +28,8 @@ always @(posedge cathode or posedge reset or posedge clk_34)
           ena<=1;
       end
   end
-assign leds=show;
+//assign leds=show;
+always @(posedge clk_34) begin
+  leds <= show;
+end
 endmodule
